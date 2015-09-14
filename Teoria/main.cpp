7ca8f88d642d7ccf9bc4e1322624e41b32fe73d2
Alex Baylon <aunesto15@gmail.com>
2015-09-14 09:19:39 -0500

    Initial Commit

---
+++ Teoria/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Teoria
+//
+//  Created by Alexander Arturo Baylon Ibanez on 14/09/15.
+//  Copyright (c) 2015 Alexander Arturo Baylon Ibanez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
